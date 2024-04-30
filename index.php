9459db9064fb5e5e70852119409f93e8914be7f0
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-30 19:33:10 +0200

    SNACK 3 completed

--- index.php
+++ index.php
@@ -32,7 +32,48 @@ $matches = [
     ],
 ];
 ?>
+<?php
 
+
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -47,6 +88,7 @@ $matches = [
 
 <body>
     <main>
+        <!-- SNACK 1 -->
         <section class="d-flex flex-column align-items-center">
             <h2 class="text-center">Snack 1</h2>
             <?php
@@ -55,15 +97,16 @@ $matches = [
             }
             ?>
         </section>
+        <!-- SNACK 2 -->
         <section class="container d-flex flex-column align-items-center">
             <h2 class="text-center pt-5">Snack 2</h2>
-            <?php
+            <!-- <?php
             if (empty($_GET['name']) || empty($_GET['email']) || empty($_GET['age'])) {
                 echo '<h5>Devi inserire un nome, un\' email e un\' età</h5>';
             } else {
-                echo '<h5>'. $name = $_GET['name'] . ' ' . $email = $_GET['email'] . ' ' . $age = $_GET['age'].'<h5>';
+                echo '<h5>' . $_GET['name'] . ' ' . $_GET['email'] . ' ' . $_GET['age'] . '<h5>';
             }
-            ?>
+            ?> -->
             <form action="index.php" method="GET">
                 <input type="text" name="name">
                 <input type="email" name="email">
@@ -72,7 +115,22 @@ $matches = [
             </form>
             <div class="container">
                 <span><?php echo (strlen($_GET['name']) < 3) ? 'Accesso negato: il nome deve avere almeno 3 caratteri' : 'Accesso consentito' ?></span>
+                <span><?php echo str_contains('@' && '.', $_GET['email']) ?></span>
             </div>
+        </section>
+        <!-- SNACK 3 -->
+        <section class="d-flex flex-column align-items-center">
+            <h2 class="text-center pt-5">Snack 3</h2>
+            <?php 
+            foreach ($posts as $post) {
+                foreach ($post as $key => $value) {
+                    echo  $value['title'] . ' ' . $value['author'] . ' ' . $value['text'] . '<br>';
+                }}
+            ?>
+        </section>
+        <section class="d-flex flex-column align-items-center">
+                <h2 class="text-center pt-5">Snack 4</h2>
+                
         </section>
     </main>
 </body>
